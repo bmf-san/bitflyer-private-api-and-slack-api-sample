ec5b6267ba8f964743a8def6b0385b613336a319
bmf <bmf.infomation@gmail.com>
2017-02-22 02:41:22 +0900

    [update ]

--- bitflyer.js
+++ bitflyer.js
@@ -1,43 +1,76 @@
 var request = require('request');
 var crypto = require('crypto');
+var async = require('async');
 
-var key = 'your_bitflyer_api';
-var secret = 'your_bitflyer_secret';
+var key = 'YOUR_KEY';
+var secret = 'YOUR_SECRET';
 
 var timestamp = Date.now().toString();
-var method = 'GET';
-var path = '/v1/me/getbalance';
+var sign = crypto.createHmac('sha256', secret).update(timestamp + 'GET' + '/v1/me/getbalance').digest('hex');
 
-var text = timestamp + method + path;
-var sign = crypto.createHmac('sha256', secret).update(text).digest('hex');
-
-var options = {
-        url: 'https://api.bitflyer.jp' + path,
-        method: method,
-        headers: {
+var requests = [{
+    url: 'https://api.bitflyer.jp/v1/me/getbalance',
+    headers: {
         'ACCESS-KEY': key,
         'ACCESS-TIMESTAMP': timestamp,
         'ACCESS-SIGN': sign,
         'Content-Type': 'application/json'
     }
-};
+}, {
+    url: 'https://api.bitflyer.jp/v1/getexecutions'
+}];
 
-request(options, function (err, response, payload) {
-    var hoge = 'hoge';
-
-    var data = JSON.stringify({"text": payload, "username": "your_bot_name", "icon_url": "your_icon_url","channel": "#channel_name"});
-
-    var options = {
-        url: 'your_slack_webhookurl',
-        form: 'payload=' + data,
-        json: true
-    };
-
-    request.post(options, function(error, response, body){
+async.map(requests, function(obj, callback) {
+    request(obj, function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body.name);
+            var body = JSON.parse(body);
+            callback(null, body);
         } else {
-            console.log('error: '+ response.statusCode + body);
+            callback(error || response.statusCode);
         }
     });
+}, function(err, results) {
+    if (err) {
+        console.log(err);
+    } else {
+        var jpy_available = parseInt(results[0][0]['available']);
+        var btc_available = results[0][1]['available'];
+        var price = parseInt(results[1][1]['price']);
+        var total_assets = Math.floor(btc_available*price);
+
+        var data = JSON.stringify({
+            "attachments": [{
+                "fallback": "bitflyer資産情報",
+                "color": "danger",
+                "title": "bitflyer資産情報",
+                "text": "現在保有しているBTCは" + btc_available + "です",
+                "fields": [{
+                    "title": "BTC総資産",
+                    "value": total_assets,
+                    "short": true
+                }, {
+                    "title": "JPY(残高)",
+                    "value": jpy_available + '円',
+                    "short": true
+                }]
+            }],
+            "username": "your-bot-name",
+            "icon_url": "/path/to/img",
+            "channel": "#yourslackchannel"
+        });
+
+        var options = {
+            url: 'https://hooks.slack.com/services/WEBHOOK_TOKEN',
+            form: 'payload=' + data,
+            json: true
+        };
+
+        request.post(options, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body.name);
+            } else {
+                console.log('error: ' + response.statusCode + body);
+            }
+        });
+    }
 });
